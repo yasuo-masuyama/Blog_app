64121c86dbf71f581fe52080a267151367d3a07c
yasuo masuyama <y.m19980526@gmail.com>
2023-06-02 11:02:23 +0900

    init

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,49 @@
+class BlogsController < ApplicationController
+  before_action :set_blog, only: %i[ show edit update destroy ]
+
+  def index
+    @blogs = Blog.all
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blogs_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @blog.update(blog_params)
+      redirect_to blogs_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
+
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
+end
